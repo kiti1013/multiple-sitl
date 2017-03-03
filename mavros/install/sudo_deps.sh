5168cd3fa27cdf07c915a83369202b6330082592
xiaowei zhao <495010593@qq.com>
2017-03-03 09:21:08 +0800

    update
    use it in indigo

--- mavros/install/sudo_deps.sh
+++ mavros/install/sudo_deps.sh
@@ -1,6 +1,6 @@
 #ROS
 
-ros_release="kinetic"
+ros_release="indigo"
 
 echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list
 apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
